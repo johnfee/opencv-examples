372f28260fba7840b6b161952daf588676153aa0
johnfee <johannes.feder.73@gmail.com>
2024-07-16 19:36:05 +0200

    some changes

--- GenerateArucoMarker/GenChArucoboard.py
+++ GenerateArucoMarker/GenChArucoboard.py
@@ -4,12 +4,8 @@ import cv2.aruco as aruco
 # Create ChArUco board, which is a set of Aruco markers in a chessboard setting
 # meant for calibration
 # the following call gets a ChArUco board of tiles 5 wide X 7 tall
-gridboard = aruco.CharucoBoard_create(
-        squaresX=5, 
-        squaresY=7, 
-        squareLength=0.04, 
-        markerLength=0.02, 
-        dictionary=aruco.Dictionary_get(aruco.DICT_5X5_1000))
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+gridboard = cv2.aruco.CharucoBoard((11, 8), 0.015, 0.011, aruco_dict)
 
 # Create an image from the gridboard
 img = gridboard.draw(outSize=(988, 1400))
